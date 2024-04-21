e2aba156ad1c5539410087864fcc5798b33e2dd7
Guillaume Sailé <saileguillaume2@gmail.com>
2024-04-21 23:54:49 +0200

    [ex00] Done (with comments)

--- ex00/adder.rs
+++ ex00/adder.rs
@@ -1,7 +1,11 @@
 pub fn adder(a: u32, b: u32) -> u32 {
+	// if no more carry, return the sum
 	if b == 0 {
 		return a;
 	}
 
+	// 1. a ^ b get the sum of the bits without the carry
+	// 2. (a & b) << 1 get the carry (in the next bit)
+	// 3. recursivity to add the carry to the sum
 	return adder(a ^ b, (a & b) << 1);
 }
